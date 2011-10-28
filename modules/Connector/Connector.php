e0a44c026005d661f510ccb9396314958cb14176
Henning Kvinnesland <henning@keyteq.no>
2011-10-28 13:53:29 +0200

    First draft complete.

--- modules/Connector/Connector.php
+++ modules/Connector/Connector.php
@@ -20,6 +20,10 @@ class Connector
     protected $username;
     /** @var string The address identifying your KeyMedia installation. */
     protected $mediabaseDomain;
+    /** @var string The class used for progress-reporting. */
+    protected $progressClass;
+    /** @var string The method used for progress reporting. */
+    protected $progressMethod;
 
     /**
      * Returns an instance of the API.
@@ -37,21 +41,103 @@ class Connector
 
     /**
      *
+     * Sets the callback used for progressreporting.
+     * The method should be static.
+     * It should also support the four following arguments:
+     * downloadsize, downloadedsize, uploadsize, uploadedsize.
+     *
+     * @param $class
+     * @param $method
+     */
+    public function setProgressCallback($class, $method)
+    {
+        $this->progressClass = $class;
+        $this->progressMethod = $method;
+    }
+
+    /**
+     *
      * Uploads an image to KeyMedia
      *
-     * @param $path
+     * @param $fieldname
+     * @param array $tags
+     * @param array $attributes
+     *
+     * @return mixed|null
      */
-    public function uploadImage($path)
+    public function uploadImage($fieldname, $tags = array(), $attributes = array())
     {
+        set_time_limit(3600);
+        ini_set('max_execution_time', '3600');
+
         $url = $this->getRequestUrl();
 
+        $filename = $_FILES[$fieldname]['tmp_name'] ?: null;
+
+        if (file_exists($filename))
+        {
+            $postFields = array
+            (
+                'image' => '@' . $filename,
+                'originalName' => $_FILES[$fieldname]['name'],
+                'tags' => serialize($tags),
+                'attributes' => serialize($attributes)
+            );
+
+            $result = $this->uploadByCurl($url, $postFields);
+
+            return $result;
+        }
+        else return null;
+    }
+
+    /**
+     *
+     * Upload alternative by curl.
+     *
+     * @param $url
+     * @param $postFields
+     *
+     * @return mixed
+     */
+    protected function uploadByCurl($url, $postFields)
+    {
         $ch = curl_init($url);
 
-        curl_setopt($ch, CURLOPT_POSTFIELDS)
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_NOPROGRESS, 0);
+        curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, array('ezkpmedia\modules\Connector\Connector', 'progressCallback'));
+        curl_setopt($ch, CURLOPT_TIMEOUT, 3600);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        echo $res;
+        $result = curl_exec($ch);
+
+        return $result;
+    }
+
+    /**
+     *
+     * Progress reporter for curl.
+     *
+     * @param $download_size
+     * @param $downloaded_size
+     * @param $upload_size
+     * @param $uploaded_size
+     */
+    public function progressCallback($download_size, $downloaded_size, $upload_size, $uploaded_size)
+    {
+        if (class_exists($this->progressClass))
+        {
+           if (method_exists($this->progressClass, $this->progressMethod))
+           {
+               $callback = array($this->progressClass, $this->progressMethod);
+               $params = array($download_size, $downloaded_size, $upload_size, $uploaded_size);
+
+               call_user_func_array($callback, $params);
+           }
+        }
     }
 
     /**
@@ -62,7 +148,7 @@ class Connector
      */
     protected function getRequestUrl()
     {
-        $url = $this->mediabaseDomain .'/media/' . '4ea94bc30c507cf80f000003/info';
+        $url = $this->mediabaseDomain .'/media/upload';
         if (strpos($url, "http") === false)
             $url = 'http://'.$url;
 
@@ -73,7 +159,7 @@ class Connector
 
         $params['username'] = $this->username;
         $params['auth'] = $auth;
-        //$params['XDEBUG_SESSION_START'] = true;
+
         $queryString = http_build_query($params);
 
         if (strpos($url, '?') === false)
--- modules/UserTest/UserTest.php
+++ modules/UserTest/UserTest.php
@@ -26,9 +26,34 @@ class UserTest
     public function execute()
     {
         $api = new Connector('ea799a1d3ee2d58690c97735d2f2571a', 'keymedia', 'km.no');
-        $api->uploadImage('');
+        $api->setProgressCallback('ezkpmedia\modules\UserTest\UserTest', 'callback');
+
+        $attributes = array('ok' => 'nei', 'godtbilde' => 'tja', 'ugyldigattributt' => 'hmm?');
+
+        $result = $api->uploadImage('image', array('tag1', 'tag2'), $attributes);
+
+        echo $result;
 
         \eZExecution::cleanExit();
+    }
+
+    /**
+     *
+     * Test of progress.
+     *
+     * @param $a
+     * @param $b
+     * @param $c
+     * @param $d
+     */
+    public static function callBack($a, $b, $c, $d)
+    {
+        /*
+        trigger_error($a);
+        trigger_error($b);
+        trigger_error($c);
+        trigger_error($d);
+        */
     }
 
     /**
@@ -36,9 +61,7 @@ class UserTest
      */
     public function viewForm()
     {
-        $tpl = \eZTemplate::factory();
-
-        echo $tpl->fetch('form.tpl');
+        require_once('form.tpl');
 
         \eZExecution::cleanExit();
     }
