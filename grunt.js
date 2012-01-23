ac3d2fea9b9c6446e65b52947f17497b8c92f425
Raymond Julin <raymond.julin@gmail.com>
2012-01-23 22:05:54 +0100

    Added grunt task for jslint

---
+++ grunt.js
@@ -0,0 +1,9 @@
+config.init({
+    lint: {
+        files: [
+            'design/standard/javascript/*.js',
+            'design/standard/javascript/models/*.js',
+            'design/standard/javascript/views/*.js'
+        ]
+    }
+});
