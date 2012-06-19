27e63cb12fc8bf49f2d398320e62bfb7b0b4e1cc
Raymond Julin <raymond.julin@gmail.com>
2012-06-19 14:17:56 +0200

    Probably fixes a bug in Media::thumb() making it not work

--- models/Media.php
+++ models/Media.php
@@ -98,7 +98,7 @@ class Media
      */
     public function thumb($width, $height)
     {
-        return 'http://' . $this->host() . "/{$width}x{$height}/{$this->_id}.jpg";
+        return 'http://' . $this->host() . "/{$width}x{$height}/{$this->id}.jpg";
     }
 
     /**
